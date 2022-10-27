2ec3e735e151957adaef7b7ffa2320293a1c2ff5
dgouron <damien.gouron@gmail.com>
2022-10-27 18:57:09 +0200

    Modifications
    - Modification de la connexion mongo
    - Delete du body parser obsolète
    - Réécriture de la fonction GetAllSauces avec async / await

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -1,60 +1,60 @@
-const Sauce = require('../models/sauces');
+const Sauce = require("../models/sauces");
 
-exports.getAllSauces = (req, res, next) => {
-    console.log('getAllSauces')
-
-    Sauce.find()
-    .then((sauces) => {
-        console.log(sauces)
-        res.status(200).json({sauces})
-    })
-    .catch(err => res.status(400).json({err}));
+exports.getAllSauces = async (req, res, next) => {
+  try {
+    const sauces = await Sauce.find();
+    console.log(sauces);
+    res.status(200).json(sauces);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 };
 
 exports.getOneSauce = (req, res, next) => {
-    Sauce.findOne({id: req.params._id})
-    .then((sauce) => res.status(200).json({sauce}))
-    .catch(err => res.status(404).json({err}));
-}; 
+  Sauce.findOne({ id: req.params._id })
+    .then((sauce) => res.status(200).json({ sauce }))
+    .catch((err) => res.status(404).json({ err }));
+};
 
 exports.postOneSauce = (req, res, next) => {
-    console.log("post")
-    const sauceObj = JSON.parse(req.body.sauce);
-    delete sauceObj._id;
-    delete sauceObj.userId;
-    const sauce = new Sauce({
-        ...sauceObj,
-        userId: req.auth.userId,
-        imageUrl: `${req.protocol}://${req.get('host')}/image/${req.file.filename}`
-    })   
-//    delete(req.body._id);
-//    const sauce = new Sauce({...req.body});
-    sauce.save()
-    .then(() => res.status(201).json({message: "New object created successfully"}))
-    .catch(err => {
-        console.log(err)
-        res.status(400).json({err});    
-})
+  console.log("post");
+  const sauceObj = JSON.parse(req.body.sauce);
+  delete sauceObj._id;
+  delete sauceObj.userId;
+  const sauce = new Sauce({
+    ...sauceObj,
+    userId: req.auth.userId,
+    imageUrl: `${req.protocol}://${req.get("host")}/image/${req.file.filename}`,
+  });
+  //    delete(req.body._id);
+  //    const sauce = new Sauce({...req.body});
+  sauce
+    .save()
+    .then(() =>
+      res.status(201).json({ message: "New object created successfully" })
+    )
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json({ err });
+    });
 };
 
 exports.putOneSauce = (req, res, next) => {
-    Sauce.updateOne({id: req.params._id}, {id: req.params._id})
-    .then(() => res.status(200).json({message: 'Object modified successfully'}))
-    .catch(err => res.status(400).json({err}));
+  Sauce.updateOne({ id: req.params._id }, { id: req.params._id })
+    .then(() =>
+      res.status(200).json({ message: "Object modified successfully" })
+    )
+    .catch((err) => res.status(400).json({ err }));
 };
 
 exports.deleteOneSauce = (req, res, next) => {
-    Sauce.deleteOne({id: require.params.id})
-    .then(() => res.status(200).json({message: 'Object deleted successfully'}))
-    .catch(err => res.status(400).json({err}));
+  Sauce.deleteOne({ id: require.params.id })
+    .then(() =>
+      res.status(200).json({ message: "Object deleted successfully" })
+    )
+    .catch((err) => res.status(400).json({ err }));
 };
 
 //compléter les logiques métier
 
-exports.postOneSauceLike = (req, res, next) => {
-
-};
-
-
-
-
+exports.postOneSauceLike = (req, res, next) => {};
--- server.js
+++ server.js
@@ -1,48 +1,56 @@
-const http = require('http');
-const app = require('./app');
+const mongoose = require("mongoose");
+const http = require("http");
+const app = require("./app");
 
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
 
-const normalizePort = val => {
-    const port = parseInt(val, 10);
-  
-    if (isNaN(port)) {
-      return val;
-    }
-    if (port >= 0) {
-      return port;
-    }
-    return false;
-  };
-  const port = normalizePort(process.env.PORT || '3000');
-  app.set('port', port);
-  
-  const errorHandler = error => {
-    if (error.syscall !== 'listen') {
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+const errorHandler = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const address = server.address();
+  const bind =
+    typeof address === "string" ? "pipe " + address : "port: " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges.");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use.");
+      process.exit(1);
+      break;
+    default:
       throw error;
-    }
-    const address = server.address();
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-    switch (error.code) {
-      case 'EACCES':
-        console.error(bind + ' requires elevated privileges.');
-        process.exit(1);
-        break;
-      case 'EADDRINUSE':
-        console.error(bind + ' is already in use.');
-        process.exit(1);
-        break;
-      default:
-        throw error;
-    }
-  };
-  
-  const server = http.createServer(app);
-  
-  server.on('error', errorHandler);
-  server.on('listening', () => {
-    const address = server.address();
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-    console.log('Listening on ' + bind);
-  });
+  }
+};
 
-server.listen(3000);
+const server = http.createServer(app);
+
+server.on("error", errorHandler);
+server.on("listening", () => {
+  const address = server.address();
+  const bind = typeof address === "string" ? "pipe " + address : "port " + port;
+  console.log("Listening on " + bind);
+});
+
+server.listen(3000, () => {
+  mongoose
+    .connect(
+      "mongodb+srv://user1:user1@coddingnes.kkmgzbd.mongodb.net/?retryWrites=true&w=majority"
+    )
+    .then(() => console.log("Connexion à MongoDB réussie !"))
+    .catch(() => console.log("Connexion à MongoDB échouée !"));
+});
